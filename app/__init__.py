9bd40cee0d9fdd4b97bf3d5c0089d8b6051fd7c7
Y3CR <yecr.juancho.1999@gmail.com>
2026-09-02 00:07:01 -0500

    API RFID funcional: registros de acceso guardados y probados

--- app/__init__.py
+++ app/__init__.py
@@ -41,6 +41,7 @@ def create_app(config_class=Config):
 
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
+    csrf.exempt(api_bp)
 
     @app.route('/')
     def index():
